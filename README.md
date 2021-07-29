3c7e0036e3f224d9533d918a35bf1d6dca64946c
Corey <corey_924@yahoo.com.tw>
2021-07-30 00:17:43 +0800

    Update Bootstrap v5.0.2

--- README.md
+++ README.md
@@ -3,18 +3,16 @@ Resume
 
 ## 用途
 
-本目錄下為本人從履歷內容擷取設計之「個人履歷網頁」，以 Bootstrap 4 試寫的自適應（RWD）頁面。
+本目錄下為本人從履歷內容擷取設計之「個人履歷網頁」，以 Bootstrap 5 試寫的自適應（RWD）頁面。
 
+### 連結
+* [https://corey924.github.io/Resume](https://corey924.github.io/Resume)
 
-# 連結
-* [https://corey924.github.io/Resume/](https://corey924.github.io/Resume/)
 
 ## Library
 
-* Bootstrap 4.4.1
-* jQuery 3.4.1
-* Font Awesome 5.12.0
-* Vue.js 2.6.11
+* Bootstrap 5.0.2
+* Font Awesome 5.15.3
 
 
 ## 設計引用
--- assets/js/freelancer.js
+++ assets/js/freelancer.js
@@ -1,63 +1,34 @@
-(function($) {
-  "use strict"; // Start of use strict
+window.addEventListener('DOMContentLoaded', event => {
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 70)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Scroll to top button appear
-  $(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 80
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-  // Floating label headings for the contact form
-  $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-      $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-      $(this).removeClass("floating-label-form-group-with-focus");
+    };
+    navbarShrink();
+    document.addEventListener('scroll', navbarShrink);
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
     });
-  });
-
-})(jQuery); // End of use strict
+});
