fb5f5bc8fc799f8ec90d2f1f43f4664af0c6e8b5
Corey <corey_924@yahoo.com.tw>
2024-12-24 17:31:50 +0800

    Update css

--- assets/js/freelancer.js
+++ assets/js/freelancer.js
@@ -50,18 +50,3 @@ window.addEventListener('DOMContentLoaded', event => {
   });
 
 });
-
-let currentLang = document.documentElement.getAttribute("lang") || "zh-Hant-TW";
-const langLabel = document.getElementById("langLabel");
-langLabel.textContent = (currentLang === "en") ? "繁中" : "EN";
-document.getElementById("langSwitchBtn").addEventListener("click", function () {
-  switch (currentLang) {
-    case "zh-Hant-TW":
-      window.location = 'index_en.html';
-      break;
-    case "en":
-    default:
-      window.location = 'index.html';
-      break;
-  }
-});
